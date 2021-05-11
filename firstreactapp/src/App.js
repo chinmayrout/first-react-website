814ad980501b0e447d91e4bd1c492d45f6459052
thechinmayrout <thechinmayrout@gmail.com>
2021-05-11 14:30:14 -0400

    Add functionality of adding items to the to do list

--- firstreactapp/src/App.js
+++ firstreactapp/src/App.js
@@ -3,10 +3,45 @@ import logo from "./chinmay_logo.png";
 import "./App.css";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newItem: "",
+      list: [],
+    };
+  }
+
+  addItem(todoValue) {
+    if (todoValue !== "") {
+      const newItem = {
+        id: Date.now(),
+        value: todoValue,
+        isDone: false,
+      };
+      const list = [...this.state.list];
+      list.push(newItem);
+
+      this.setState({
+        list,
+        newItem: "",
+      });
+    }
+  }
+
+  deleteItem(id) {
+    const list = [...this.state.list];
+    const updatedlist = list.filter((item) => item.id !== id);
+    this.setState({ list: updatedlist });
+  }
+
+  updateInput(input) {
+    this.setState({ newItem: input });
+  }
+
   render() {
     return (
       <div>
-        <img src={logo} width="150" height="150" className="logo" />
+        <img src={logo} width="20%" height="100%" className="logo" />
         <h1 className="app-title">To-Do App</h1>
         <div className="container">
           Add an Item...
@@ -15,14 +50,42 @@ class App extends React.Component {
             type="text"
             className="input-txt"
             placeholder="Write a To-Do"
+            required
+            value={this.state.newItem}
+            onChange={(e) => this.updateInput(e.target.value)}
           />
-          <button className="add-btn">Add TODO</button>
+          <button
+            className="add-btn"
+            onClick={() => this.addItem(this.state.newItem)}
+            disabled={!this.state.newItem.length}
+          >
+            Add TODO
+          </button>
           <div className="list">
             <ul>
+              {this.state.list.map((item) => {
+                return (
+                  <li key={item.id}>
+                    <input
+                      type="checkbox"
+                      name="idDone"
+                      checked={item.isDone}
+                      onChange={() => {}}
+                    />
+                    {item.value}
+                    <button
+                      className="btn"
+                      onCLick={() => this.deleteItem(item.id)}
+                    >
+                      Delete
+                    </button>
+                  </li>
+                );
+              })}
               <li>
                 <input type="checkbox" name="" id="" />
                 Do Some Homework
-                <button className="btn"> Delete  </button>
+                <button className="btn"> Delete </button>
               </li>
             </ul>
           </div>
