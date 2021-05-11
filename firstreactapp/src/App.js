77aad6a1a34aaf729b7bf2be7de3c4248d09bfc5
thechinmayrout <thechinmayrout@gmail.com>
2021-05-11 14:00:48 -0400

    Added class and made changes to the App.css

--- firstreactapp/src/App.js
+++ firstreactapp/src/App.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./chinmay_logo.png";
+import "./App.css";
 
-
-function App(){
-  return (
-    <div className ="App">  {/*use className not class */}
-      <header className = "App-header">
-        <img src={logo} className='App-logo' />
-        <p> Chinmay's first React App</p>
-        </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <img src={logo} width="150" height="150" className="logo" />
+        <h1 className="app-title">To-Do App</h1>
+        <div className="container">
+          Add an Item...
+          <br />
+          <input
+            type="text"
+            className="input-txt"
+            placeholder="Write a To-Do"
+          />
+          <button className="add-btn">Add TODO</button>
+          <div className="list">
+            <ul>
+              <li>
+                <input type="checkbox" name="" id="" />
+                Do Some Homework
+                <button className="btn"> Delete  </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
